f5001cbd76297fb88f2cf01cdf0505d5e95cd5c9
Harshvardhan Pandit <me@harshp.com>
2024-01-30 22:02:40 +0000

    mouse natural scroll script for multiple devices

--- scripts/bin/mouse_scroll_natural.sh
+++ scripts/bin/mouse_scroll_natural.sh
@@ -13,27 +13,27 @@ debug() {
 }
 
 # Identify wireless mouse
-connected_device_id=$(
-    xinput list |
-    grep "Mouse" | grep -v "DLL" |
-    sed -n 's/.*id=\([0-9]\+\).*/\1/p'
-)
+connected_devices=$(xinput list | grep "Mouse" | grep -v "DLL")
+
 # If no mouse is connected, quit
-if [[ -z $connected_device_id ]]; then
+if [[ -z $connected_devices ]]; then
     echo "no wireless mouse is connected"
     exit 1;
 fi
-debug "device id = $connected_device_id"
-
-# Identify property associated with natural scrolling
-natural_scrolling_property=$(
-    xinput list-props "${connected_device_id}" |
-    grep "Natural Scrolling" | 
-    head -n 1 | 
-    sed -n 's/.*(\([0-9]\+\)).*/\1/p'
-)
-debug "scrolling property = $natural_scrolling_property"
+# debug "devices = $connected_devices"
 
 # Set natural scrolling for device
-xinput set-prop $connected_device_id $natural_scrolling_property 1
+while IFS= read -r line; do
+    connected_device_id=$(echo $line | sed -n 's/.*id=\([0-9]\+\).*/\1/p')
+    debug "device = $connected_device_id"
+    # Identify property associated with natural scrolling
+    natural_scrolling_property=$(
+        xinput list-props "${connected_device_id}" |
+        grep "Natural Scrolling" | 
+        head -n 1 | 
+        sed -n 's/.*(\([0-9]\+\)).*/\1/p'
+    )
+    debug "scrolling property = $natural_scrolling_property"
+    xinput set-prop $connected_device_id $natural_scrolling_property 1
+done <<< $connected_devices 
 debug "natural scrolling set"
