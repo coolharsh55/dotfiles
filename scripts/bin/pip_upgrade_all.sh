1ec1b33575bcc16496be1b35e90913effd5116a6
Harshvardhan Pandit <me@harshp.com>
2025-03-15 14:47:20 +0000

    scripts: fix pip_upgrade

--- scripts/bin/pip_upgrade_all.sh
+++ scripts/bin/pip_upgrade_all.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # upgrades all pip packages
 # author: Harshvardhan J. Pandit
-pip install --upgrade $(pip list -ol | tail -n +3 | awk '{print }')
+pip install --upgrade $(pip list -ol | tail -n +3 | awk '{print $1}')
